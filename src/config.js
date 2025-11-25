edc4f1dcb0c3c5a8ca0b8c26af67fe181fc0e562
Vanessa <vanessabkrs@gmail.com>
2025-11-25 16:57:05 +0100

    deployed the FE to AWS

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
-  const BACKEND_URL = import.meta.env.VITE_API_BACKEND_URL;
+  // const BACKEND_URL = import.meta.env.VITE_API_BACKEND_URL;
+
+  // const BACKEND_URL = "http://localhost:8080";
+
+  const BACKEND_URL = "http://a4bbbb29ee5194856b386779cd04a484-1759277395.eu-central-1.elb.amazonaws.com";
   
   export { BACKEND_URL };
