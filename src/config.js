79900ad26d0834a117ad3b672927694a10e0246d
Vanessa <vanessabkrs@gmail.com>
2025-12-16 14:44:17 +0100

    added admin pwd for testing

--- src/config.js
+++ src/config.js
@@ -4,4 +4,6 @@
 
   // const BACKEND_URL = "http://a8a6fc24784af4b6192865f084e22c47-1082690188.eu-central-1.elb.amazonaws.com";
   
+const admin_pwd = "Admin@12345"; // VULNERABILITY #1: A01:2021 - Broken Access Control - Hardcoded Credentials
+
   export { BACKEND_URL };
