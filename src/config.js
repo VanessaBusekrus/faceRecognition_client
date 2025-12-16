2f83cdd9004d98167435f5b80d0db0700e6ca81e
Vanessa <vanessabkrs@gmail.com>
2025-12-16 14:31:04 +0100

    added vulnerability so password is printed when user enters it wrongly

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
   // const BACKEND_URL = import.meta.env.VITE_API_BACKEND_URL;
 
-  // const BACKEND_URL = "http://localhost:8080";
+  const BACKEND_URL = "http://localhost:8080";
 
-  const BACKEND_URL = "http://a8a6fc24784af4b6192865f084e22c47-1082690188.eu-central-1.elb.amazonaws.com";
+  // const BACKEND_URL = "http://a8a6fc24784af4b6192865f084e22c47-1082690188.eu-central-1.elb.amazonaws.com";
   
   export { BACKEND_URL };
