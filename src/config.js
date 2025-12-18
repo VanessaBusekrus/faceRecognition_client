d6b27faed7d4883051fbdde7ae71a987833b576e
Vanessa <vanessabkrs@gmail.com>
2025-12-18 13:59:45 +0100

    adjusted config.js to call env vars and added .env file for scerets

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
-  // const BACKEND_URL = import.meta.env.VITE_API_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_API_BACKEND_URL;
 
-  const BACKEND_URL = "http://localhost:8080";
+const ADMIN_PWD= import.meta.env.VITE_ADMIN_PWD;
 
-  // const BACKEND_URL = "http://a8a6fc24784af4b6192865f084e22c47-1082690188.eu-central-1.elb.amazonaws.com";
-  
-const admin_pwd = "Admin@12345"; // VULNERABILITY #1: A01:2021 - Broken Access Control - Hardcoded Credentials
-
-  export { BACKEND_URL, admin_pwd };
+export { BACKEND_URL, ADMIN_PWD };
